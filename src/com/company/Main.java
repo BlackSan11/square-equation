4a89973f2ff0bef7c46dd7807a83ab1bd0d6f732
Bogdan <bhelpdig@gmail.com>
2017-11-16 19:42:02 +0300

    решений при b = 0 не имеет

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        getRoots(0, 45, 5454);
+        getRoots(0, 2, 5);
     }
 
     //ищем дискриминант
@@ -13,22 +13,26 @@ public class Main {
     //ищем корни
     public static void getRoots(int a, int b, int c) {
         int d = discrimenant(a, b, c);
-        double x1;
         if (a != 0) {
             if (d < 0) {
                 System.out.println("Уровнение корней не имеет");
             } else if (d == 0) {
-                x1 = (-b) / (2 * a);
+                double x1 = (-b) / (2 * a);
                 System.out.println("Уровнение имеет один корень, x = " + x1);
             } else {
                 double dsqrt = Math.sqrt(d);
-                x1 = ((-b) + dsqrt) / (2 * a);
+                double x1 = ((-b) + dsqrt) / (2 * a);
                 double x2 = ((-b) - dsqrt) / (2 * a);
                 System.out.println("Уровнение имеет два корня, x1 = " + x1 + ", х2 = " + x2);
             }
         } else {
-            x1 = -c / b;
-            System.out.println("Уровнение имеет один корень, x = " + x1);
+            if (b == 0){
+                System.out.println("Уровнение корней не имеет");
+            }else{
+                double x1 = (double) -c / b;
+                System.out.println("Уровнение имеет один корень, x = " + x1);
+            }
+
         }
 
     }
