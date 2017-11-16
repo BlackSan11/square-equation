e3ae15ff4a5ec97554cf460598fb4374312a1915
Bogdan <bhelpdig@gmail.com>
2017-11-16 19:14:32 +0300

    Исправил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,34 @@
 package com.company;
-// ax2 + bx + c = 0, a!=0
-// D = b^2 -4ac
-// Если D < 0, корней нет;
-// Если D = 0, есть ровно один корень;
-// Если D > 0, корней будет два.
-// x1 = -b+(D)/2a
-// x2 = -b-(D)/2a
+
 public class Main {
     public static void main(String[] args) {
-        getRoots(0,0,0);
+        getRoots(0, 45, 5454);
     }
+
     //ищем дискриминант
-    public static double discrimenant(double a, double b, double c){
-        double d = b * b - 4 * a * c;
-        return d;
+    public static int discrimenant(int a, int b, int c) {
+        return b * b - 4 * a * c;
     }
+
     //ищем корни
-    public static void getRoots(double a, double b, double c){
-        double d = discrimenant(a,b,c);
-        double x1,x2;
-        if(a != 0){
-            if(d < 0){
+    public static void getRoots(int a, int b, int c) {
+        int d = discrimenant(a, b, c);
+        double x1;
+        if (a != 0) {
+            if (d < 0) {
                 System.out.println("Уровнение корней не имеет");
-            }
-            else if(d == 0){
-                x1 = ((-b) + Math.sqrt(d)) / (2 * a);
+            } else if (d == 0) {
+                x1 = (-b) / (2 * a);
                 System.out.println("Уровнение имеет один корень, x = " + x1);
-            }
-            else if(d > 0){
-                x1 = ((-b) + Math.sqrt(d)) / (2 * a);
-                x2 = ((-b) - Math.sqrt(d)) / (2 * a);
+            } else {
+                double dsqrt = Math.sqrt(d);
+                x1 = ((-b) + dsqrt) / (2 * a);
+                double x2 = ((-b) - dsqrt) / (2 * a);
                 System.out.println("Уровнение имеет два корня, x1 = " + x1 + ", х2 = " + x2);
             }
-            else{
-                System.out.println("Err.. (Что-то пошло не так)");
-            }
-        }
-        else{
-            System.out.println("Err.. (Деление на 0)");
+        } else {
+            x1 = -c / b;
+            System.out.println("Уровнение имеет один корень, x = " + x1);
         }
 
     }
